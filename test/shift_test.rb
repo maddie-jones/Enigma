a92bf03a16d779612d53aee5be15e2cf1827745b
Maddie Jones <maddyg91@gmail.com>
2018-11-02 20:44:10 -0600

    added first test for shift

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,5 +2,11 @@ require_relative 'test_helper'
 require './lib/shift'
 
 class ShiftTest < Minitest::Test
-  
+  def setup
+    @shift = Shift.new("02715","040895")
+  end
+
+  def test_it_exists
+    assert_instance_of Shift, @shift
+  end
 end
