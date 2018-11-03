9f23e7ee2e612c8e5ea39b95b88ab9596ad8566d
Maddie Jones <maddyg91@gmail.com>
2018-11-03 11:40:45 -0600

    date offset passing tests

--- lib/shift.rb
+++ lib/shift.rb
@@ -17,6 +17,16 @@ class Shift
     # refactor as loop with group_by
   end
 
-
+  def date_offset
+    date_squared = @date.to_i ** 2
+    date_squared_index = date_squared.to_s.length - 1
+    square_four_digit = date_squared.to_s[-4..date_squared_index]
+    {
+      "A"=> square_four_digit[0].to_i,
+      "B" => square_four_digit[1].to_i,
+      "C" => square_four_digit[2].to_i,
+      "D" => square_four_digit[3].to_i
+    }
+  end
 
 end
