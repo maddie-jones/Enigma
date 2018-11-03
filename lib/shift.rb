35216da8e940a63e04a01fec3cfe449006e893e1
Maddie Jones <maddyg91@gmail.com>
2018-11-02 20:47:49 -0600

    added tests to shif class, passing

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,3 +1,10 @@
 class Shift
-  
+
+  attr_reader :key, :date
+
+  def initialize(key, date)
+    @key = key
+    @date = date
+  end
+
 end
--- test/shift_test.rb
+++ test/shift_test.rb
@@ -9,4 +9,13 @@ class ShiftTest < Minitest::Test
   def test_it_exists
     assert_instance_of Shift, @shift
   end
+
+  def test_it_has_key
+    assert_equal "02715", @shift.key
+  end
+
+  def test_it_has_date
+    assert_equal "040895", @shift.date
+  end
+  
 end
