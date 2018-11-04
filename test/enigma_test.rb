46f432e710b2ddef4a95c290b52a18949caefc11
Maddie Jones <maddyg91@gmail.com>
2018-11-04 15:33:59 -0700

    removed direction from encrypt and decrypt method, giving it in the new_encrypted_message method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,7 +17,7 @@ class EnigmaTest < Minitest::Test
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, @enigma.encrypt("Hello world!", "02715", "040895", "encrypt")
+    assert_equal expected, @enigma.encrypt("Hello world!", "02715", "040895")
   end
 
   def test_it_can_decrypt
@@ -26,7 +26,7 @@ class EnigmaTest < Minitest::Test
       key: "02715",
       date: "040895"
     }
-    assert_equal expected, @enigma.decrypt("keder ohulw!", "02715", "040895", "decrypt")
+    assert_equal expected, @enigma.decrypt("keder ohulw!", "02715", "040895")
   end
 
 end
