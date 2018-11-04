16d8e755c7026d80ebbe0d6ace6851b28e75c525
Maddie Jones <maddyg91@gmail.com>
2018-11-04 10:40:12 -0700

    tests for enigma.encrypt passsing

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,20 @@
 require './lib/message'
 require './lib/key'
 require './lib/shift'
+require './lib/encryptor'
 class Enigma
 
   def encrypt(message, key = nil, date = nil)
     message = Message.new(message)
-    key = Key.new(key)
     shift = Shift.new(key, date)
+    indexed_message = message.cell_index_values
+    shift= Shift.new(key, date)
+    encryptor = Encryptor.new(indexed_message, shift)
+    return {
+      encryption: encryptor.new_encrypted_message,
+      key: key,
+      date: shift.date
+      }
   end
 
 end
