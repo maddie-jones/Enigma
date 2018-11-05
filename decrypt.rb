98f0bbb20844986ec3ac7c98e9fb135ffd80022f
Maddie Jones <maddyg91@gmail.com>
2018-11-05 11:03:08 -0700

    CLI working correctly

--- decrypt.rb
+++ decrypt.rb
@@ -1,13 +1,13 @@
 require './lib/enigma'
+require 'pry'
 file = File.open(ARGV[0], "r")
 file_2 = File.open(ARGV[1], "w")
 message = file.read.split("\n")
 enigma = Enigma.new
-key = Key.new.number
-date = Time.now.strftime("%d%m%y")
-
+key = ARGV[2]
+date = ARGV[3]
 message.each do |line|
-  file_2.puts(enigma.encrypt(line, key, date)[:encryption])
+  file_2.puts(enigma.decrypt(line, key, date)[:decryption])
 end
 
 puts "Created #{ARGV[1]} with the key #{key} and date #{date}"
